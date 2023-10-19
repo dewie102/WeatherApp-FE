7c70d112cb4395904658ff912be67bcecaccb7ed
Zack Romasz <zack.romasz@tlgcohort.com>
2023-10-18 22:36:41 -0400

    Got places autocomplete 'working'

--- script.js
+++ script.js
@@ -1,98 +1,124 @@
+let PlacesService;
+
 // Get weatherApp cookies
 const cookieValue = document.cookie
-  .split("; ")
-  .find((row) => row.startsWith("weatherAppCookie="))
-  ?.split("=")[1];
+    .split("; ")
+    .find((row) => row.startsWith("weatherAppCookie="))
+    ?.split("=")[1];
 
 // Generate the list of previously searched zip codes
 let previouslySearched = function previouslySearched() {
-  let previouslySearched = cookieValue.split(",");
+    let previouslySearched = cookieValue.split(",");
 
-  const previouslySearchedDiv = document.getElementById(
-    "previouslySearchedDiv"
-  );
-  previouslySearchedDiv.replaceChildren();
-  previouslySearchedDiv.appendChild(document.createElement("h6"));
-  previouslySearchedDiv.lastChild.textContent = "Previous Searches:";
-  for (let element of previouslySearched) {
-    let newLocation = document.createElement("a");
-    newLocation.classList.add("text-decoration-none");
-    newLocation.classList.add("search-history");
-    newLocation.value = element;
-    newLocation.onclick = function () {
-      // TODO: make a call to the function that gets the new location from the weather api
-    };
-    newLocation.textContent = element;
-    previouslySearchedDiv.appendChild(newLocation);
-  }
+    const previouslySearchedDiv = document.getElementById(
+        "previouslySearchedDiv"
+    );
+    previouslySearchedDiv.replaceChildren();
+    previouslySearchedDiv.appendChild(document.createElement("h6"));
+    previouslySearchedDiv.lastChild.textContent = "Previous Searches:";
+    for (let element of previouslySearched) {
+        let newLocation = document.createElement("a");
+        newLocation.classList.add("text-decoration-none");
+        newLocation.classList.add("search-history");
+        newLocation.value = element;
+        newLocation.onclick = function () {
+            // TODO: make a call to the function that gets the new location from the weather api
+        };
+        newLocation.textContent = element;
+        previouslySearchedDiv.appendChild(newLocation);
+    }
 };
 
 // event listener that checks the location search field
 locationSearch.addEventListener("keypress", (e) => {
-  let searchValue = document.getElementById("locationSearch").value;
+    let searchValue = document.getElementById("locationSearch").value;
 
-  if (e.key === "Enter") {
-    if (!cookieValue) {
-      console.log(cookieValue);
-      document.cookie = `weatherAppCookie=${searchValue}`;
-    } else {
-      console.log(cookieValue);
+    if (e.key === "Enter") {
+        if (!cookieValue) {
+            console.log(cookieValue);
+            document.cookie = `weatherAppCookie=${searchValue}`;
+        } else {
+            console.log(cookieValue);
 
-      document.cookie = `weatherAppCookie=${searchValue},${cookieValue}`;
+            document.cookie = `weatherAppCookie=${searchValue},${cookieValue}`;
 
-      getLocationFromZip(searchValue);
+            getLocationFromZip(searchValue);
+        }
+        previouslySearched;
     }
-    previouslySearched;
-  }
 });
 
 window.onload = previouslySearched;
 
 function getLocationFromZip(zipcode) {
-  try {
-    fetch(
-      `http://localhost:3000/api/weatherapp/openweather/zip?zipcode=${zipcode}`
-    )
-      .then((res) => res.json())
-      .then((content) => {
-        if (Object.hasOwn(content, "error")) {
-          console.log(content.error);
-          return content;
-        }
+    try {
+        fetch(
+            `http://localhost:3000/api/weatherapp/openweather/zip?zipcode=${zipcode}`
+        )
+            .then((res) => res.json())
+            .then((content) => {
+                if (Object.hasOwn(content, "error")) {
+                    console.log(content.error);
+                    return content;
+                }
 
-        return content;
-      })
-      .then(processReturnedLocationValue);
-  } catch (error) {
-    console.log(error);
-  }
+                return content;
+            })
+            .then(processReturnedLocationValue);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 function processReturnedLocationValue(locationValue) {
-  if (Object.hasOwn(locationValue, "error")) {
-    alert(locationValue.error);
-    return;
-  }
+    if (Object.hasOwn(locationValue, "error")) {
+        alert(locationValue.error);
+        return;
+    }
 
-  if (!locationValue.lat && !locationValue.lon) {
-    console.log("Something went wrong, we don't have both values");
-    return;
-  }
+    if (!locationValue.lat && !locationValue.lon) {
+        console.log("Something went wrong, we don't have both values");
+        return;
+    }
 
-  saveLonAndLatToLocalStorage({
-    lat: locationValue.lat,
-    lon: locationValue.lon,
-  });
+    saveLonAndLatToLocalStorage({
+        lat: locationValue.lat,
+        lon: locationValue.lon,
+    });
 }
 
 function saveLonAndLatToLocalStorage({ lat, lon }) {
-  localStorage.setItem("lat", lat);
-  localStorage.setItem("lon", lon);
+    localStorage.setItem("lat", lat);
+    localStorage.setItem("lon", lon);
 }
 
 function getLonAndLatFromLocalStorage() {
-  const lat = localStorage.getItem("lat");
-  const lon = localStorage.getItem("lon");
+    const lat = localStorage.getItem("lat");
+    const lon = localStorage.getItem("lon");
 
-  return { lat, lon };
+    return { lat, lon };
+}
+
+PlacesService;
+
+async function initPlaces() {
+    PlacesService = await google.maps.importLibrary("places");
+
+    const options = {
+        componentRestrictions: { country: "us" },
+        fields: ["address_components", "geometry", "icon", "name"],
+    };
+
+    const autocomplete = new PlacesService.Autocomplete(
+        document.getElementById("locationSearch"),
+        options
+    );
+
+    google.maps.event.addListener(autocomplete, "place_changed", () => {
+        let place = autocomplete.getPlace();
+        console.log(place);
+        console.log(place.geometry.location);
+        console.log(place.geometry.location.lat());
+        console.log(place.geometry.location.lng());
+    });
 }
