5750687ec3e05c6e81d4d279dd4f020beb60a527
Robert Martinez <robertez@amazon.com>
2023-10-19 08:56:49 -0500

    updated favorites, login, and signup page

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ const cookieValue = document.cookie
   .find((row) => row.startsWith("weatherAppCookie="))
   ?.split("=")[1];
 
+let isLoggedIn = true;
+
+// TODO: if logged in get and display the favorites and place them in the favoritesDiv div
+if (isLoggedIn) {
+  for (let i = 0; i < 5; i++) {
+    let newFavorite = document.createElement("img");
+    newFavorite.src = "https://via.placeholder.com/150";
+    newFavorite.alt = "Favorite";
+    newFavorite.classList.add("img-thumbnail");
+    newFavorite.style.margin = "5px";
+    favoritesDiv.appendChild(newFavorite);
+  }
+}
 // Generate the list of previously searched zip codes
 let previouslySearched = function previouslySearched() {
   let previouslySearched = cookieValue.split(",");
@@ -29,25 +42,24 @@ let previouslySearched = function previouslySearched() {
 
 // event listener that checks the location search field
 locationSearch.addEventListener("keypress", (e) => {
-  let searchValue = document.getElementById("locationSearch").value;
+  console.log("test");
+  let searchValue = document.getElementById("locationSearch");
 
   if (e.key === "Enter") {
-    if (!cookieValue) {
-      console.log(cookieValue);
-      document.cookie = `weatherAppCookie=${searchValue}`;
-    } else {
-      console.log(cookieValue);
-
-      document.cookie = `weatherAppCookie=${searchValue},${cookieValue}`;
-
-      getLocationFromZip(searchValue);
+    const locationSearch = getLocationFromZip(searchValue.value);
+    if (!cookieValue && locationSearch !== undefined) {
+      document.cookie = `weatherAppCookie=${searchValue.value}`;
+    } else if (locationSearch !== undefined) {
+      if (!cookieValue.includes(searchValue.value)) {
+        document.cookie = `weatherAppCookie=${searchValue.value},${cookieValue}`;
+      }
     }
-    previouslySearched;
+    getCurrentWeather();
+    getAirPollution();
   }
 });
 
-window.onload = previouslySearched;
-
+// get location from zipcode
 function getLocationFromZip(zipcode) {
   try {
     fetch(
@@ -68,12 +80,14 @@ function getLocationFromZip(zipcode) {
   }
 }
 
+// process the returned location value
 function processReturnedLocationValue(locationValue) {
   if (Object.hasOwn(locationValue, "error")) {
     alert(locationValue.error);
     return;
   }
 
+  console.log(locationValue);
   if (!locationValue.lat && !locationValue.lon) {
     console.log("Something went wrong, we don't have both values");
     return;
@@ -85,14 +99,127 @@ function processReturnedLocationValue(locationValue) {
   });
 }
 
+// save lon and lat to local storage
 function saveLonAndLatToLocalStorage({ lat, lon }) {
   localStorage.setItem("lat", lat);
   localStorage.setItem("lon", lon);
 }
 
+// get lon and lat from local storage
 function getLonAndLatFromLocalStorage() {
   const lat = localStorage.getItem("lat");
   const lon = localStorage.getItem("lon");
 
   return { lat, lon };
 }
+
+// get current weather (Only returns the current weather)
+async function getCurrentWeather() {
+  const { lat, lon } = await getLonAndLatFromLocalStorage();
+
+  const response = await fetch(
+    `http://localhost:3000/api/weatherapp/openweather?lat=${lat}&lon=${lon}`
+  );
+  const data = await response.json();
+  createWeatherCard(data);
+}
+
+// get air pollution
+async function getAirPollution() {
+  const { lat, lon } = await getLonAndLatFromLocalStorage();
+  let airQualityDiv = document.getElementById("airQualityDiv");
+
+  const response = await fetch(
+    `http://localhost:3000/api/weatherapp/openweather/airpollution?lat=${lat}&lon=${lon}`
+  );
+  const data = await response.json();
+  let qualityText = document.createElement("h2");
+  let qualityTitle = document.createElement("h1");
+  qualityText.classList.add("text-center");
+  qualityText.textContent = `${data.airPollution}`;
+  airQualityDiv.replaceChildren();
+  airQualityDiv.appendChild(qualityTitle);
+  qualityTitle.classList.add("text-center");
+  qualityTitle.textContent = "Air Quality";
+  airQualityDiv.appendChild(qualityText);
+}
+
+// create weather card
+function createWeatherCard(content) {
+  let weatherCard = document.createElement("div");
+  weatherCard.classList.add("card");
+  weatherCard.style.width = "18rem";
+  let weatherCardImg = document.createElement("img");
+  weatherCardImg.classList.add("card-img-top");
+  weatherCardImg.src = content.iconUrl;
+  weatherCardImg.alt = "weather icon";
+  weatherCard.appendChild(weatherCardImg);
+  let mainWeatherDiv = document.getElementById("mainWeatherDiv");
+  mainWeatherDiv.replaceChildren();
+
+  let weatherCardBody = document.createElement("div");
+  weatherCardBody.classList.add("card-body");
+  weatherCardBody.innerHTML = `
+  <hr/>
+  <p>Temp: ${content.temp}</p>
+  <p>Wind Speed: ${content.windSpeed}</p>
+  <p>Humidity: ${content.humidity}</p>
+  `;
+  weatherCard.appendChild(weatherCardBody);
+  mainWeatherDiv.appendChild(weatherCard);
+}
+
+(function getWeatherAlerts() {
+  // https://api.weather.gov/alerts/active
+  fetch("https://api.weather.gov/alerts/active")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.features);
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+})();
+
+// function pollutionChart(pollution) {
+//   google.charts.load("current", { packages: ["bar"] });
+//   google.charts.setOnLoadCallback(drawStuff);
+
+//   function drawStuff() {
+//     var data = new google.visualization.arrayToDataTable([
+//       ["", "Percentage"],
+//       ["co", 138.52],
+//       ["np", 0],
+//       ["no2", 0.31],
+//       ["o3", 70.81],
+//       ["so2", 0.09],
+//       ["pm2_5", 10.44],
+//       ["pm10", 31.2],
+//       ["nh3", 0.58],
+//     ]);
+
+//     var options = {
+//       title: "Air Pollution",
+//       width: 900,
+//       legend: { position: "none" },
+//       chart: { title: "Air Pollution Chart" },
+//       bars: "horizontal", // Required for Material Bar Charts.
+//       axes: {
+//         x: {
+//           0: { side: "top", label: "" }, // Top x-axis.
+//         },
+//       },
+//       bar: { groupWidth: "90%" },
+//     };
+
+//     var chart = new google.charts.Bar(document.getElementById("top_x_div"));
+//     chart.draw(data, options);
+//   }
+// }
+
+window.onload = previouslySearched;
